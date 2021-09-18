9b6491ad03357f774b18aba56873ed5390dbda0b
VincentSastra <vincent.sastra@gmail.com>
2021-09-18 14:25:59 -0700

    Backend: Create blob utility function

--- backend/blob/blob.js
+++ backend/blob/blob.js
@@ -7,8 +7,6 @@ exports.uploadBlob = async (content, contentType = "txt") => {
 	// Create a container beforehand and put the name in the .env
 	const containerClient = blobServiceClient.getContainerClient(process.env.AZURE_BLOB_CONTAINER_NAME);
 
-	let content = "helloWorld"
-
 	const blobName = uuidv4() + "." + contentType;
 	const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 	const uploadBlobResponse = await blockBlobClient.upload(content, content.length);
