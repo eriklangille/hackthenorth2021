d97957322e7f81c351d4fcdcbd8fc43d0ee399b8
eriklangille <eriklangille@gmail.com>
2021-09-18 00:42:49 -0700

    reminders page

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+
+const reminders = require('./routes/reminders');
+
 const app = express()
 const port = 3000
+
+app.use('/reminders', reminders);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
