2bcc2a8bb188606fd008ed82eff874e27e9f0620
VincentSastra <vincent.sastra@gmail.com>
2021-09-18 01:12:14 -0700

    Backend: Install Nodemon for debugging

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 
-const reminders = require('./routes/reminders');
+const registration = require('./routes/registration')
+const reminders = require('./routes/reminders')
 
 const app = express()
 const port = 3000
 
+app.use('/registration', registration)
 app.use('/reminders', reminders);
 
 app.get('/', (req, res) => {
