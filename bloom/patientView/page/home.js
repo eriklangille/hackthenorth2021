90cd230fd1a6f761afe181663351a3b5dc651788
scottlangille <scottrlangille@gmail.com>
2021-09-18 15:38:51 -0700

    Indentation

--- bloom/patientView/page/home.js
+++ bloom/patientView/page/home.js
@@ -7,17 +7,17 @@ function Home() {
             <div id="date">Friday, September 17, 2021</div>
             <div id="title">Good morning, Mary</div>
             <div className="nav">
-            <div className="nav-button" id="nav-family">Family tree</div>
-            <div className="nav-button" id="nav-p1"></div>
-            <div className="nav-button" id="nav-p2"></div>
-            <div className="nav-button" id="nav-p3"></div>
+                <div className="nav-button" id="nav-family">Family tree</div>
+                <div className="nav-button" id="nav-p1"></div>
+                <div className="nav-button" id="nav-p2"></div>
+                <div className="nav-button" id="nav-p3"></div>
             </div>
             <div className="checklist">
-            <ChecklistItem status={true} name="Breakfast" time="6:30am"></ChecklistItem>
-            <ChecklistItem status={false} name="Morning medication" time="10:00am"></ChecklistItem>
-            <ChecklistItem status={false} name="Lunch" time=""></ChecklistItem>
-            <ChecklistItem status={false} name="Dinner" time=""></ChecklistItem>
-            <ChecklistItem status={false} name="Night medication" time=""></ChecklistItem>
+                <ChecklistItem status={true} name="Breakfast" time="6:30am"></ChecklistItem>
+                <ChecklistItem status={false} name="Morning medication" time="10:00am"></ChecklistItem>
+                <ChecklistItem status={false} name="Lunch" time=""></ChecklistItem>
+                <ChecklistItem status={false} name="Dinner" time=""></ChecklistItem>
+                <ChecklistItem status={false} name="Night medication" time=""></ChecklistItem>
             </div>
         </div>
     );
