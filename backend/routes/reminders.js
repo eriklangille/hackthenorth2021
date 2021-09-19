ec58c37288a0d332d9d01b40bc3979f3afed4ede
VincentSastra <vincent.sastra@gmail.com>
2021-09-19 03:39:33 -0700

    Make Checklist Responsive

--- backend/routes/reminders.js
+++ backend/routes/reminders.js
@@ -20,11 +20,13 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   const title = req.body.title
   const userId = req.body.userId;
+  const targetDate = req.body.targetDate;
   await sql.connect(config).then(pool => {
     return pool.request()
       .input('title', sql.VarChar(255), title)
+      .input('targetDate', sql.DateTime, targetDate)
       .input('userId', sql.Int, parseInt(userId))
-      .query("insert into todo(title, userId, createDate) values (@title, @userId, getdate())")
+      .query("insert into todo(title, userId, targetDate, createDate) values (@title, @userId, @targetDate, getdate())")
   }).then(() => {
     res.sendStatus(200);
   }).catch(() => {
@@ -33,13 +35,26 @@ router.post("/", async (req, res) => {
 });
 
 // @route GET /reminders/:id
-// @desc Retrieve a specific reminder
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
+// @desc Retrieve all relevant reminder for user
+router.get("/:user", async (req, res) => {
+  const { user } = req.params;
   await sql.connect(config).then(pool => {
     return pool.request()
-      .input('id', sql.Int, parseInt(id))
-      .query("select * from todo where id = @id")
+      .input('userId', sql.Int, parseInt(user))
+      .query(`
+      select * from todo as t
+      where userId = @userId
+      and (
+      t.completeDate is null
+      or 1 > (
+        select count(1)
+        from todo as t1
+        where userId = @userId
+        and t1.completeDate is not null
+        and t1.completeDate > t.completeDate )
+      )
+      order by createDate asc
+      `)
   }).then(result => {
     return res.json(result.recordset);
   })
@@ -51,17 +66,20 @@ router.post("/:id", async (req, res) => {
   const { id } = req.params;
   const title = req.body.title;
   const setComplete = req.body.setComplete;
+  const dateCompleted = req.body.dateCompleted
   await sql.connect(config).then(pool => {
     if (setComplete) {
       return pool.request()
         .input('id', sql.Int, parseInt(id))
         .input('title', sql.VarChar(255), title)
+        .input('completeDate', sql.DateTime, dateCompleted)
         .query("update todo set title = @title, completeDate = getdate() where id = @id")
+    } else {
+      return pool.request()
+        .input('id', sql.Int, parseInt(id))
+        .input('title', sql.VarChar(255), title)
+        .query("update todo set title = @title, completeDate = null where id = @id")
     }
-    return pool.request()
-      .input('id', sql.Int, parseInt(id))
-      .input('title', sql.VarChar(255), title)
-      .query("update todo set title = @title, completeDate = null where id = @id")
   }).then(result => {
     return res.json(result.recordset);
   })
--- bloom/src/components/FamilyChecklist.js
+++ bloom/src/components/FamilyChecklist.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import ChecklistItem from "./ChecklistItem"
+import { ChecklistItem } from "./ChecklistItem"
 import { backendEndpoint } from "../static"
 import { familyId } from "../Utils/ids"
 import './Family.scss'
@@ -9,12 +9,7 @@ const FamilyChecklist = () => {
 	return (
 		<div class="phone horizontally-centered">
 			<div className="checklist">
-                <ChecklistItem status={true} name="Breakfast" time="6:30am"></ChecklistItem>
-                <ChecklistItem status={false} name="Morning medication" time="10:00am"></ChecklistItem>
-                <ChecklistItem status={false} name="Lunch" time=""></ChecklistItem>
-                <ChecklistItem status={false} name="Dinner" time=""></ChecklistItem>
-                <ChecklistItem status={false} name="Night medication" time=""></ChecklistItem>
-            </div>
+			</div>
 			<div class="background-image"><img src="../bloom-background.svg"></img></div>
 		</div>
 	)
