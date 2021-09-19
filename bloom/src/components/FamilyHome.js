43a0f2962db58108a7fba821a896f911a7a66dea
scottlangille <scottrlangille@gmail.com>
2021-09-19 02:07:13 -0700

    Static family homepage

--- bloom/src/components/FamilyHome.js
+++ bloom/src/components/FamilyHome.js
@@ -11,12 +11,14 @@ function logout() {
 
 const FamilyHome = () => {
 	return (
-		<div class="phone horizontally-centered vertically-centered">
-			<div>test</div>
+		<div class="phone horizontally-centered">
 			<div class="logo"><img src='../bloom-logo.svg'></img></div>
+			<div class="relative-photo"></div>
+			<div class="relative-name">Mary</div>
+			<div class="relative-description">Send Mary some love by sharing a recent photo and attaching a note.</div>
 			<div class="selection-screen">
-				<div><Link to="/family/login"><input type="button" value="button1" class="primary-button" /></Link></div>
-				<div><Link to="/family/register"><input type="button" value="button2" class="secondary-button" /></Link></div>
+				<div><Link to="#"><input type="button" value="Send photo" class="primary-button" /></Link></div>
+				<div><Link to="#"><input type="button" value="Edit daily checklist" class="secondary-button" /></Link></div>
 			</div>
 			<div class="footer-button">All done? <Link to="/family" onClick={logout}>Sign out here.</Link></div>
 			<div class="background-image"><img src="../bloom-background.svg"></img></div>
