21a72e0e889cd2ce4368553f5382ea64d5dc0e25
eriklangille <eriklangille@gmail.com>
2021-09-19 04:12:50 -0700

    dynamic gallery

--- bloom/src/patient/page/photo.js
+++ bloom/src/patient/page/photo.js
@@ -2,23 +2,49 @@ import React, { useState, useEffect } from 'react'
 import {TransitionGroup, CSSTransition} from 'react-transition-group';
 import PhotoItem from '../../components/photoItem';
 import './photo.scss'
+import { userId } from '../../Utils/ids';
+import { getPhotoUrls } from '../../Utils/photo';
 
 const newPhoto = (photo, text, author, date) => {
   return {Key: Math.random(0, 100000), Photo: photo, Text: text, Author: author, Date: date}
 }
 
-const defPhoto = (text) => {
-  return newPhoto("./photo1.jpg", `Hi ${text}! Beautiful evening. Love you always ❤`, "From your son Michael", "Aug 29, 2021")
+const defPhoto = (text="Mom", image="./photo1.jpg", date = "2021-09-19T07:31:26.990Z") => {
+  let useimage = image || "./photo1.jpg"
+  let options = {year: 'numeric', month: 'short', day: 'numeric' };
+  let infodate  = new Date(date || "2021-09-12T07:31:26.990Z");
+
+  let stringDate = infodate.toLocaleDateString("en-US", options);
+  return newPhoto(useimage, `Hi ${text}! Beautiful evening. Love you always ❤`, "From your son Michael", stringDate)
 }
 
 let firstLoad = false
 
 const Photo = () => {
   const [photos, setPhotos] = useState([])
+  const [photoData, setPhotoData] = useState([])
+  const [photoNumbers, setPhotoNumbers] = useState([0, 4])
   const [xy, setXY] = useState({x: 0, y:0})
   const [mouseDown, setMouseDown] = useState(false)
   const [debounce, setDebounce] = useState(false)
   const [mouseXY, setMouseXY] = useState({x: 0, y:0})
+
+  const updatePhotoNumber = (numbers, diff) => {
+    let new_nums = [...numbers]
+    for (let i = 0; i < new_nums.length; i++) {
+      new_nums[i] += diff;
+      if(new_nums[i] < 0)
+      {
+        new_nums[i] = photoData.length - 1
+      }
+      else if(new_nums[i] >= photoData.length)
+      {
+        new_nums[i] = 0
+      }
+    }
+    console.log(new_nums)
+    setPhotoNumbers(new_nums)
+  }
 
   const handleMouseMove = (event) => {
     setXY({
@@ -31,11 +57,13 @@ const Photo = () => {
     if (!debounce && mouseDown) {
       if (xy.x - mouseXY.x >= 60)
       {
-        setPhotos([defPhoto("Mother"), ...photos.slice(0,-1)]);
+        updatePhotoNumber(photoNumbers, -1)
+        setPhotos([defPhoto("Mother", photoData[photoNumbers[0]]?.urlString), ...photos.slice(0,-1)]);
         setDebounce(true)
       }
       if (xy.x - mouseXY.x <= -60) {
-        setPhotos([...photos.slice(1), defPhoto("Mother")]);
+        updatePhotoNumber(photoNumbers, 1)
+        setPhotos([...photos.slice(1), defPhoto("Mother", photoData[photoNumbers[0]]?.urlString)]);
         setDebounce(true)
       }
     }
@@ -52,7 +80,15 @@ const Photo = () => {
   }
 
   useEffect(() => {
-    setPhotos([defPhoto("Mom"), defPhoto("ats"), defPhoto("dad"), defPhoto("Mother"), defPhoto("Mother")])
+    localStorage.setItem(userId, 330421523)
+    async function fetchData() {
+      let photoData = await getPhotoUrls(userId)
+      setPhotoData(photoData)
+      console.log(`DATA: ${photoData}`)
+      console.table(photoData)
+      setPhotos([defPhoto("Mom", photoData[0]?.urlString), defPhoto("Mom", photoData[1]?.urlString), defPhoto("Mom", photoData[2]?.urlString), defPhoto("Mother", photoData[3]?.urlString), defPhoto("Mother", photoData[4]?.urlString)])
+    }
+    fetchData();
   }, [])
 
   return (
